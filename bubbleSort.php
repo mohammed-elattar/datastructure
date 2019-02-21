7850b61b1ca35f10c50aac79d2587f7c5049aabb
Mohamed Said Elattar <mseel3ttar@gmail.com>
2019-02-21 10:01:37 +0200

    Optimize bubble sort

--- bubbleSort.php
+++ bubbleSort.php
@@ -5,13 +5,18 @@ ini_set('display_errors', 1);
 function bubbleSort(&$arr)
 {
   $n = sizeof($arr);
+  $swap = false;
   for ($i = 0;$i < $n;$i++) {
     for ($j = 0; $j < $n - $i - 1; $j++) {
       if ($arr[$j] > $arr[$j + 1]) {
         $temp = $arr[$j];
         $arr[$j] = $arr[$j + 1];
         $arr[$j + 1] = $temp;
+        $swap = true;
       }
+    }
+    if(!$swap){
+    break;
     }
   }
   }
