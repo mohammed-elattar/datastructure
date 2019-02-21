bbbc4f309a692319704cf394bcd651711e1fe433
Mohamed Said Elattar <mseel3ttar@gmail.com>
2019-02-21 09:53:27 +0200

    Create Bubble Sort

---
+++ bubbleSort.php
@@ -0,0 +1,22 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+function bubbleSort(&$arr)
+{
+  $n = sizeof($arr);
+  for ($i = 0;$i < $n;$i++) {
+    for ($j = 0; $j < $n - $i - 1; $j++) {
+      if ($arr[$j] > $arr[$j + 1]) {
+        $temp = $arr[$j];
+        $arr[$j] = $arr[$j + 1];
+        $arr[$j + 1] = $temp;
+      }
+    }
+  }
+  }
+  $arr = array(64, 34, 25, 12, 22, 11, 90);
+  bubbleSort($arr);
+
+var_dump($arr);
+
